bcad379526e314e23ba32da3a76904ea0f87ee06
minhyeok1232 <160010293+minhyeok1232@users.noreply.github.com>
2025-03-28 21:37:01 +0900

    Feat : "Object Pool Init Method"

--- TDS_Project/Assets/2. Scripts/ObjectPooler.cs
+++ TDS_Project/Assets/2. Scripts/ObjectPooler.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class ObjectPooler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static ObjectPooler instance;
+    
+    [SerializeField] private GameObject monsterPrefab;
+    [SerializeField] private int        monsterPoolSize;
+    
+    private Queue<GameObject> pool = new Queue<GameObject>();
+
+
+    
     void Start()
     {
-        
+        InitMonsterPool();
+    }
+
+    void InitMonsterPool()
+    {
+        for (int i = 0; i < monsterPoolSize; i++)
+        {
+            GameObject obj = Instantiate(monsterPrefab);
+            obj.SetActive(false);
+            obj.tag = Helpers.Monster.Monster_Tag;
+            pool.Enqueue(obj);
+        }
     }
 
     // Update is called once per frame
